faab156a8481cab96bb90ccd03e2a174b0a71e67
Gabriel Vander <gabsvander@hotmail.com>
2022-04-16 15:42:10 -0300

    Implemented unit tests for GetRandomWordByDifficultyUseCase

--- app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/hangman/core/domain/use_cases/GetRandomWordByDifficultyUseCase.kt
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/hangman/core/domain/use_cases/GetRandomWordByDifficultyUseCase.kt
@@ -1,16 +1,18 @@
 package br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.use_cases
 
 import arrow.core.Either
-import br.edu.ifsp.aluno.vander.gabriel.hangman.core.data.repositories.WordRepositoryInMemoryImpl
+import br.edu.ifsp.aluno.vander.gabriel.hangman.core.data.repositories.WordRepositoryFactory
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.entities.Difficulty
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.entities.Word
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.failures.Failure
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.repositories.WordRepository
 
-class GetRandomWordByDifficultyUseCase {
-    private val wordRepository: WordRepository = WordRepositoryInMemoryImpl()
+class GetRandomWordByDifficultyUseCase(
+    private val wordRepository: WordRepository = WordRepositoryFactory.build()
+) {
 
     suspend fun execute(difficulty: Difficulty): Either<Failure, Word> {
         return wordRepository.getSingleWordByDifficulty(difficulty)
     }
+
 }
