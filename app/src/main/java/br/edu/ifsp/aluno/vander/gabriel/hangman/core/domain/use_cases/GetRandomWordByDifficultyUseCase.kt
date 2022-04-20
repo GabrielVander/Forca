cd362e9dbb7ac42301abc2df978680a2bf7cb35b
Gabriel Vander <gabsvander@hotmail.com>
2022-04-20 00:12:42 -0300

    Switched application data source to remote

--- app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/hangman/core/domain/use_cases/GetRandomWordByDifficultyUseCase.kt
+++ app/src/main/java/br/edu/ifsp/aluno/vander/gabriel/hangman/core/domain/use_cases/GetRandomWordByDifficultyUseCase.kt
@@ -1,6 +1,7 @@
 package br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.use_cases
 
 import arrow.core.Either
+import br.edu.ifsp.aluno.vander.gabriel.hangman.config.Source
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.data.repositories.WordRepositoryFactory
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.entities.Difficulty
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.entities.Word
@@ -8,7 +9,7 @@ import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.failures.Failure
 import br.edu.ifsp.aluno.vander.gabriel.hangman.core.domain.repositories.WordRepository
 
 class GetRandomWordByDifficultyUseCase(
-    private val wordRepository: WordRepository = WordRepositoryFactory.build()
+    private val wordRepository: WordRepository = WordRepositoryFactory.build(Source.REMOTE)
 ) {
 
     suspend fun execute(difficulty: Difficulty): Either<Failure, Word> {
